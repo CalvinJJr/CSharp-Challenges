d83e2e465560514c5fae350e5ea75059d934558b
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-24 00:29:25 -0400

    Add files via upload

---
+++ Squaring Program/Program.cs
@@ -0,0 +1,13 @@
+﻿//Creating a squaring program
+
+static void mySquare()
+{
+    Console.WriteLine("Please enter a number to square:");
+    
+    int square = Convert.ToInt16(Console.ReadLine());
+    int squared = square * square;
+
+    Console.WriteLine($"Your number is: {squared} squared!");
+}
+
+mySquare();
