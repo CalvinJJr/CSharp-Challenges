f4d0962d7a062c62980103d6ec6768abcf9e6d1f
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-26 10:45:06 -0400

    Add files via upload

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,11 @@
+﻿//Array always requires [] brackets. Each item has to share the same variable type.
+
+string [] boxes = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+//The foreach loop accesses each item in an array, using a temporary variable (of the same type)
+
+foreach (string item in boxes)
+if (item.StartsWith("B"))
+{
+    Console.WriteLine($"Potential item {item} has been found.");
+}
