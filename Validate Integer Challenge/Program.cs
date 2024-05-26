dddf94f954ae1431872a54512e7f916541743228
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-26 10:48:07 -0400

    Add files via upload

---
+++ Validate Integer Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿string? readInput;
+int userInput = 0;
+Console.WriteLine("Please enter a number between 5 and 10.");
+
+do
+{
+    readInput = Console.ReadLine();
+    int.TryParse(readInput, out userInput);
+
+    if (userInput < 5 )
+    {
+        Console.WriteLine("The number that you've entered is invalid, please try again.");
+    }
+    else if (userInput > 10)
+        {
+            Console.WriteLine("The number that you've entered is invalid, please try again.");
+        }
+        else
+            {
+                Console.WriteLine("Your value has been accepted, thank you!");
+            }
+}
+while (userInput > -1);
+    readInput = Console.ReadLine();
+        
+
