c785779c098a9442275ef3e2f4a38365b13522a2
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-26 11:07:08 -0400

    Add files via upload

---
+++ For Loop/Program.cs
@@ -0,0 +1,7 @@
+﻿//For Loop Challenge
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
