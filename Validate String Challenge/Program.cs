5effd3ef0b33959d3ae216fe82a59e3d07702421
Calvin James, Jr <cjames.jr@outlook.com>
2024-05-27 10:52:59 -0400

    Add files via upload

---
+++ Validate String Challenge/Program.cs
@@ -0,0 +1,45 @@
+﻿//Validate String Challenge
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine(@"Please enter your role (Administrator, Manager, or User):");
+string? readResult;
+string verified = "Administrator, Manager, User";
+string nextResult;
+string cutResult;
+string finalResult;
+
+
+do
+{
+
+    readResult = Console.ReadLine();
+    nextResult = readResult.ToLower();
+    cutResult = nextResult.Trim();
+    finalResult = cutResult;
+
+
+    if (finalResult.Contains("administrator"))
+    {
+        Console.WriteLine("Your entry has been accepted.");
+    }
+
+        else if (finalResult.Contains("manager"))
+        {
+            Console.WriteLine("Your entry has been accepted.");
+        }
+
+            else if (finalResult.Contains("user"))
+            {
+                Console.WriteLine("Your entry has been accepted.");
+            }
+
+                else
+                {
+                    Console.WriteLine("Please enter a valid response.");
+                }
+
+}while(finalResult != verified);
+
+        
+
