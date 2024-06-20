5da542d6d243320222f1fd2fb4e6dd75e2a7c000
Calvin James, Jr <cjames.jr@outlook.com>
2024-06-20 12:44:00 -0400

    Add files via upload

---
+++ Data Type Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+decimal sum = 0m;
+string remainder = "";
+
+foreach (var i in values)
+{
+    decimal intValue; 
+    if (decimal.TryParse(i, out intValue))
+    {
+        sum += intValue;
+    }
+    else
+    {
+        remainder += i;
+    }
+}
+
+Console.WriteLine($"Remainder: {remainder}");
+Console.WriteLine($"Sum: {sum}");
