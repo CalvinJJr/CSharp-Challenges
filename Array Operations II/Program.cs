ae046ff3752d5c472f4119bdd0df236580df5393
Calvin James, Jr <cjames.jr@outlook.com>
2024-07-06 11:13:33 -0400

    Add files via upload

---
+++ Array Operations II/Program.cs
@@ -0,0 +1,14 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderID = orderStream.Split(',');
+Array.Sort(orderID);
+int i;
+
+for (i = 0; i < orderID.Length; i++)
+{
+    if (orderID[i].Length != 4)
+    {
+        orderID[i] = orderID[i] + " - Error";
+    }
+    
+    Console.WriteLine(orderID[i]);
+}
